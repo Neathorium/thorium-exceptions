14f3ce7beec6b945f2b7a6c85bee559e2b94a996
NEAThorium-Team <90842673+NEAThorium-Team@users.noreply.github.com>
2021-10-07 02:49:48 +0200

    Adding initial files. (#1)

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+# Initial update
+Added the initial files.
+This includes:
+- A few functions to check against Exceptions,
+- An exception that's basically the null object stand-in for Exceptions,
+- A record to hold some exception data
+
+Also added the required tests.
+Javadoc remains to be done.
---
+++ src/main/java/com/neathorium/thorium/exceptions/namespaces/ExceptionFunctions.java
@@ -0,0 +1,29 @@
+package com.neathorium.thorium.exceptions.namespaces;
+
+import com.neathorium.thorium.exceptions.constants.ExceptionConstants;
+import com.neathorium.thorium.exceptions.records.ExceptionData;
+
+import java.util.Objects;
+
+public interface ExceptionFunctions {
+    private static <T extends Exception> boolean isReferenceOrMessageEquals(T exception, T expected) {
+        return (
+            Objects.equals(expected, exception) ||
+            Objects.equals(expected.getMessage(), exception.getMessage())
+        );
+    }
+
+    static boolean isException(Exception ex) {
+        final var exception = ExceptionConstants.EXCEPTION;
+        return !(Objects.isNull(ex) || isReferenceOrMessageEquals(ex, exception));
+    }
+
+    static boolean isNonException(Exception ex) {
+        final var exception = ExceptionConstants.EXCEPTION;
+        return !Objects.isNull(ex) && isReferenceOrMessageEquals(ex, exception);
+    }
+
+    static RuntimeException getRuntimeExcpetion(ExceptionData<RuntimeException> data, Exception exception) {
+        return data.CONSTRUCTOR().apply(data.MESSAGE() + exception.getMessage(), exception);
+    }
+}
