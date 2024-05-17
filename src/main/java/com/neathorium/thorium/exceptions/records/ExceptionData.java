07bfced1c54ea16382d3a9718c523b2b9cd54826
karsaii <istvan.karsai.public@gmail.com>
2024-05-17 08:20:40 +0200

    Updating jitpack to use 21 out of the box.

--- src/main/java/com/neathorium/thorium/exceptions/records/ExceptionData.java
+++ src/main/java/com/neathorium/thorium/exceptions/records/ExceptionData.java
@@ -2,5 +2,7 @@ package com.neathorium.thorium.exceptions.records;
 
 import java.util.function.BiFunction;
 
-public record ExceptionData<ExceptionType>(BiFunction<String, Throwable, ExceptionType> CONSTRUCTOR, String MESSAGE) {
-}
+public record ExceptionData<ExceptionType>(
+    BiFunction<String, Throwable, ExceptionType> CONSTRUCTOR,
+    String MESSAGE
+) {}
